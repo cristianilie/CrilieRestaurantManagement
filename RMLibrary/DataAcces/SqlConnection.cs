fa143d29ad7a1871e111606ff7c43574df33d05b
Ilie Cristian Marius <Ilie_Cristian_Marius@hotmail.com>
2019-12-08 11:47:33 +0200

    Added the connection string to the database and started implementing the database item creation/insertion.

---
+++ RMLibrary/DataAcces/SqlConnection.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using RMLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMLibrary.DataAcces
+{
+    public class SqlConnection
+    {
+        private const string dbName = "RMS";
+
+        public  string ConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        /// <summary>
+        /// Creates a new Category in the database, in the Category Table
+        /// </summary>
+        /// <param name="model">The new category model</param>
+        /// <returns></returns>
+        public CategoryModel CreateCategory(CategoryModel model)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(ConnectionString(dbName)))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@Name", model.Name);
+                parameters.Add("@Id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spCategory_Insert", parameters, commandType: CommandType.StoredProcedure);
+
+                model.Id = parameters.Get<int>("@Id");
+
+                return model;
+            }
+        }
+    }
+}
